52090207ba51313874a274eac8cee4a40eb3adde
shaahmed115 <shaahmed115@gmail.com>
2016-08-23 22:54:34 +0200

    Test routing for book appointment

--- index.js
+++ index.js
@@ -17,4 +17,8 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+app.get('/register', function(request, response) {
+                response.render('pages/register');
+				                });
+
 
